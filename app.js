7a4151c788ef7645a32679a8e4e52a9cb43f6646
BhumeshBrillboxCoder <bhumesh.k@brillbox.ai>
2023-11-08 18:10:10 +0530

    new featurea added

--- app.js
+++ app.js
@@ -9,28 +9,41 @@ const io = new Server(server);
 
 const bodyparser = require('body-parser');
 const path = require('path');
-
 const meetingRoute = require('./routes/meetingRoute');
 
 app.use(express.json());
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: true }));
 
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
 app.set('views',path.join(__dirname,'./views'));
 app.use(express.static('public'));
 
 app.use('/',meetingRoute);
 
 io.on('connection', (socket) => {
-    socket.on('join-room', (roomId, userId) => {
-        socket.join(roomId);
-        socket.to(roomId).broadcast.emit('user-connected', userId);
+    console.log(`a user connceted ${socket.id}`);
 
-        socket.on('disconnect', () => {
-            socket.to(roomId).broadcast.emit('user-disconnected', userId);
-        });
+    socket.on('offer', offer => {
+        socket.broadcast.emit('offer', offer);
     });
+
+    socket.on('answer', answer => {
+        socket.broadcast.emit('answer', answer);
+    });
+
+    socket.on('new-ice-candidate', iceCandidate => {
+        socket.broadcast.emit('ice-candidate', iceCandidate);
+    });
+
+    socket.on('screenOffer',(screenOffer)=>{
+        socket.broadcast.emit('screenOffer', screenOffer);
+    });
+
+    socket.on('disconnect',()=>{
+        console.log('a user disconnected');
+    });
+
 });
 
 server.listen(port, () => {
--- public/script.js
+++ public/script.js
@@ -1,3 +1,4 @@
+const socket = io();
 tippy('#microphone-btn', {
     content: "Turn off microphone",
     arrow: false,
@@ -23,7 +24,109 @@ tippy('#record-btn', {
 tippy('#call-btn', {
     content: "Leave call",
     arrow: false
+});
 
+let mesgflag = true;
+const mesgICon = document.getElementById('Mesg');
+const video_div = document.getElementById('video-div');
+const video_container = document.getElementById('video-container');
+const video_container2 = document.getElementById('video-container2');
+
+document.getElementById('message-btn').addEventListener('click', () => {
+    if (mesgflag) {
+        mesgICon.classList.remove('bi-chat-left-text');
+        mesgICon.classList.add('bi-chat-left-text-fill');
+        mesgflag = false;
+    } else {
+        mesgICon.classList.remove('bi-chat-left-text-fill');
+        mesgICon.classList.add('bi-chat-left-text');
+        mesgflag = true;
+    };
+});
+
+document.querySelector('.btn-close').addEventListener('click', () => {
+    mesgICon.classList.remove('bi-chat-left-text-fill');
+    mesgICon.classList.add('bi-chat-left-text');
+    mesgflag = true;
+});
+
+let recordflag = true;
+const recordICon = document.getElementById('Record');
+
+document.getElementById('record-btn').addEventListener('click', () => {
+    if (recordflag) {
+        recordICon.classList.remove('bi-record-btn');
+        recordICon.classList.add('bi-record-btn-fill');
+        recordflag = false;
+    }
+    else {
+        recordICon.classList.remove('bi-record-btn-fill');
+        recordICon.classList.add('bi-record-btn');
+        recordflag = true;
+    };
+});
+
+const localVideo = document.getElementById('video-container2');
+const remoteVideo = document.getElementById('video-container');
+let userStream;
+let rtcPeerConnection;
+
+navigator.mediaDevices.getUserMedia({
+    video: {
+        width: 1280,
+        height: 720,
+    },
+    audio: true,
+}).then(stream => {
+    userStream = stream;
+    localVideo.srcObject = stream;
+    localVideo.play();
+
+    const iceServers = {
+        iceServers: [
+            { urls: 'stun:stun.l.google.com:19302' },
+            { urls: "stun:stun.services.mozilla.com" }
+        ],
+    };
+    const peerConnection = new RTCPeerConnection(iceServers);
+    rtcPeerConnection = peerConnection;
+    stream.getTracks().forEach(track => peerConnection.addTrack(track, stream));
+
+    socket.on('offer', offer => {
+        peerConnection.setRemoteDescription(new RTCSessionDescription(offer));
+        peerConnection.createAnswer()
+            .then(answer => peerConnection.setLocalDescription(answer))
+            .then(() => {
+                socket.emit('answer', peerConnection.localDescription);
+            });
+    });
+    socket.on('answer', answer => {
+        peerConnection.setRemoteDescription(new RTCSessionDescription(answer));
+    });
+
+    socket.on('ice-candidate', iceCandidate => {
+        peerConnection.addIceCandidate(new RTCIceCandidate(iceCandidate));
+    });
+
+    peerConnection.onicecandidate = event => {
+        if (event.candidate) {
+            socket.emit('new-ice-candidate', event.candidate);
+        }
+    };
+
+    peerConnection.ontrack = event => {
+        remoteVideo.srcObject = event.streams[0];
+        remoteVideo.play();
+    };
+
+    peerConnection.createOffer()
+        .then(offer => peerConnection.setLocalDescription(offer))
+        .then(() => {
+            socket.emit('offer', peerConnection.localDescription);
+        });
+
+}).catch(error => {
+    console.error('Error accessing media devices:', error);
 });
 
 let micFlag = true;
@@ -33,6 +136,7 @@ document.getElementById('microphone-btn').addEventListener('click', () => {
     const tippyTooltip = document.querySelector('.tippy-content');
 
     if (micFlag) {
+        userStream.getTracks()[0].enabled = false;
         micIcon.classList.remove('bi-mic-fill');
         micIcon.classList.add('bi-mic-mute-fill');
         document.getElementById('microphone-btn').classList.add('bg-danger');
@@ -41,6 +145,7 @@ document.getElementById('microphone-btn').addEventListener('click', () => {
         }
         micFlag = false;
     } else {
+        userStream.getTracks()[0].enabled = true;
         micIcon.classList.remove('bi-mic-mute-fill');
         document.getElementById('microphone-btn').classList.remove('bg-danger');
         micIcon.classList.add('bi-mic-fill');
@@ -51,7 +156,6 @@ document.getElementById('microphone-btn').addEventListener('click', () => {
     }
 });
 
-
 let videoFlag = true;
 const videoIcon = document.getElementById('Video');
 
@@ -59,81 +163,82 @@ document.getElementById('camera-btn').addEventListener('click', () => {
     const tippyTooltip = document.querySelector('.tippy-content');
 
     if (videoFlag) {
+        userStream.getTracks()[1].enabled = false;
         videoIcon.classList.remove('bi-camera-video');
         videoIcon.classList.add('bi-camera-video-off');
         document.getElementById('camera-btn').classList.add('bg-danger');
         if (tippyTooltip) {
             tippyTooltip.innerHTML = "Turn on video";
         }
-
         videoFlag = false;
     } else {
+        userStream.getTracks()[1].enabled = true;
         videoIcon.classList.remove('bi-camera-video-off');
         document.getElementById('camera-btn').classList.remove('bg-danger')
         videoIcon.classList.add('bi-camera-video');
         if (tippyTooltip) {
             tippyTooltip.innerHTML = "Turn off video";
         }
-
         videoFlag = true;
     }
 });
 
-
-let mesgflag = true;
-const mesgICon = document.getElementById('Mesg');
-const video_div = document.getElementById('video-div');
-const video_container = document.getElementById('video-container');
-const video_container2 = document.getElementById('video-container2');
-
-document.getElementById('message-btn').addEventListener('click',()=>{
-    if(mesgflag){
-        mesgICon.classList.remove('bi-chat-left-text');
-        mesgICon.classList.add('bi-chat-left-text-fill');
-        mesgflag = false;
-    }else{
-        mesgICon.classList.remove('bi-chat-left-text-fill');
-        mesgICon.classList.add('bi-chat-left-text');
-        mesgflag = true;
-    };
-});
-
-document.querySelector('.btn-close').addEventListener('click',()=>{
-    mesgICon.classList.remove('bi-chat-left-text-fill');
-    mesgICon.classList.add('bi-chat-left-text');
-    mesgflag = true;
-});
-
-
 let screenflag = true;
 const screenICon = document.getElementById('Screen');
 
-document.getElementById('screen-btn').addEventListener('click',()=>{
-    if(screenflag){
+document.getElementById('screen-btn').addEventListener('click', async() => {
+    if (screenflag) {
         screenICon.classList.remove('bi-display');
         screenICon.classList.add('bi-display-fill');
         screenflag = false;
+
+        const stream = await navigator.mediaDevices.getDisplayMedia({ video: true, audio: true });
+        remoteVideo.srcObject = stream;
+
+        stream.getVideoTracks()[0].onended = async() => {
+            remoteVideo.srcObject = userStream;
+
+            rtcPeerConnection.getSenders().forEach(sender => {
+                const track = sender.track;
+                if (track && track.kind === 'video') {
+                    rtcPeerConnection.removeTrack(sender);
+                }
+            });
+            userStream.getVideoTracks().forEach(videoTrack => {
+                rtcPeerConnection.addTrack(videoTrack, userStream);
+            });
+            const offer = await rtcPeerConnection.createOffer();
+            await rtcPeerConnection.setLocalDescription(offer);
+
+            socket.emit('screenOffer', offer);
+        };
+        const offer = await createOffer(stream);
+        socket.emit('screenOffer', offer);
     }
-    else{
+    else {
         screenICon.classList.remove('bi-display-fill');
         screenICon.classList.add('bi-display');
         screenflag = true;
     };
 });
 
+async function createOffer(stream) {
+    stream.getTracks().forEach(track => rtcPeerConnection.addTrack(track, stream));
 
-let recordflag = true;
-const recordICon = document.getElementById('Record');
+    const offer = await rtcPeerConnection.createOffer();
+    await rtcPeerConnection.setLocalDescription(offer);
 
-document.getElementById('record-btn').addEventListener('click',()=>{
-    if(recordflag){
-        recordICon.classList.remove('bi-record-btn');
-        recordICon.classList.add('bi-record-btn-fill');
-        recordflag = false;
-    }
-    else{
-        recordICon.classList.remove('bi-record-btn-fill');
-        recordICon.classList.add('bi-record-btn');
-        recordflag = true;
-    };
+    return offer;
+};
+
+socket.on('screenOffer', async(offer)=>{
+    rtcPeerConnection.setRemoteDescription(offer);
+
+    const answer = await rtcPeerConnection.createAnswer();
+    await rtcPeerConnection.setLocalDescription(answer);
+
+    socket.emit('answer', answer);
+
+    rtcPeerConnection.ontrack = OnTrackFunction;
+    rtcPeerConnection.onicecandidate = OnIceCandidateFunction;
 });
